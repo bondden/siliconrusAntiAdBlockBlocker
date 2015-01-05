f451ba643211bce416fb19780916f02c45d4d079
bondden <bond.den@gmail.com>
2015-01-06 01:01:58 +0700

    Update and download URLs added

--- siliconrus.user.js
+++ siliconrus.user.js
@@ -4,9 +4,9 @@
 // @include     http://siliconrus.com/*
 // @grant       none
 // @run-at      document-start
-// @version     0.1
-// @downloadURL 
-// @updateURL   
+// @version     0.1.1
+// @downloadURL https://github.com/bondden/siliconrusAntiAdBlockBlocker/raw/master/siliconrus.user.js
+// @updateURL   https://github.com/bondden/siliconrusAntiAdBlockBlocker/raw/master/siliconrus.user.js
 // ==/UserScript==
 
 var s=document.createElement('style');
