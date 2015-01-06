8ac456c658379fa5091048f345687a3250b775f1
bondden <bond.den@gmail.com>
2015-01-06 22:17:37 +0700

    The XCFD logo an button are removed.

--- siliconrus.user.js
+++ siliconrus.user.js
@@ -4,11 +4,28 @@
 // @include     http://siliconrus.com/*
 // @grant       none
 // @run-at      document-start
-// @version     0.1.1
+// @version     0.1.2
 // @downloadURL https://github.com/bondden/siliconrusAntiAdBlockBlocker/raw/master/siliconrus.user.js
 // @updateURL   https://github.com/bondden/siliconrusAntiAdBlockBlocker/raw/master/siliconrus.user.js
 // ==/UserScript==
 
-var s=document.createElement('style');
-s.innerHTML='.filter-grayscale{filter:none;}';
-document.getElementsByTagName('head').item(0).appendChild(s);
+//1. remove anti-adblock filter
+var bondden_gS=document.createElement('style');
+bondden_gS.innerHTML='.filter-grayscale{filter:none;}';
+document.getElementsByTagName('head').item(0).appendChild(bondden_gS);
+
+//execute, when DOM is ready
+function bondden_gInit(){
+	
+	//2. remove XCFD logo and button
+	var xcfd=document.getElementsByClassName('b-xcfd__about').item(0);
+	if(xcfd)xcfd.parentNode.removeChild(xcfd);
+
+}
+
+var bondden_gRi = setInterval(function(){
+	if(document.readyState==="complete"){
+		bondden_gInit();
+		clearInterval(bondden_gRi);
+	}
+},10);
